e28af83e3489ed977fba86cd105fcd5cefb43fbb
davileao <davileao@gmail.com>
2023-07-27 14:05:09 -0300

    Adding animation explicit and implicit

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,7 +6,7 @@ import 'package:mealsapp/widgets/category_grid_item.dart';
 import '../models/category.dart';
 import '../models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
     // required this.onToggleFavorite,
@@ -16,8 +16,37 @@ class CategoriesScreen extends StatelessWidget {
   // final void Function(Meal meal) onToggleFavorite;
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      // lowerBound: 0,
+      // upperBound: 1,
+      // this is the default value
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -34,7 +63,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
+    final Widget content = GridView(
       padding: const EdgeInsets.all(24),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -60,6 +89,28 @@ class CategoriesScreen extends StatelessWidget {
             },
           )
       ],
+    );
+
+    // example of using AnimatedBuilder - explicit animation
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: content,
+      builder: (context, child) => SlideTransition(
+        position: Tween<Offset>(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        ),
+        child: child,
+      ),
+      //   Padding(
+      // padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+      // child: child,
+      // ),
     );
   }
 }
--- lib/screens/meals_details.dart
+++ lib/screens/meals_details.dart
@@ -12,11 +12,11 @@ class MealDetailsScreen extends ConsumerWidget {
   });
 
   final Meal meal;
+
   // final void Function (Meal meal) onToggleFavorite;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final isFavorite = ref.watch(favoriteMealsProvider).contains(meal);
 
     return Scaffold(
@@ -26,34 +26,55 @@ class MealDetailsScreen extends ConsumerWidget {
           IconButton(
             onPressed: () {
               // onToggleFavorite(meal);
-              final wasAdded =
-              ref.read(favoriteMealsProvider.notifier).toggleMealFavoriteStatus(meal);
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
               ScaffoldMessenger.of(context).clearSnackBars();
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
-                  content: Text(wasAdded ? 'Added to favorites' : 'Removed from favorites'),
+                  content: Text(wasAdded
+                      ? 'Added to favorites'
+                      : 'Removed from favorites'),
                   duration: const Duration(seconds: 2),
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+
+            // add implicit animation:
+            // icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(turns: Tween(
+                  begin: 0.8,
+                  end: 1.0,
+                ).animate(animation), child: child);
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 16),
             Text(
               'Ingredients',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     color: Theme.of(context).colorScheme.primary,
-                fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.bold,
                   ),
             ),
             const SizedBox(height: 8),
@@ -68,7 +89,7 @@ class MealDetailsScreen extends ConsumerWidget {
                       style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                             color: Theme.of(context).colorScheme.onBackground,
                           ),
-                        textAlign: TextAlign.center,
+                      textAlign: TextAlign.center,
                     ),
                   const SizedBox(height: 16),
                 ],
@@ -77,19 +98,20 @@ class MealDetailsScreen extends ConsumerWidget {
             Text(
               'Steps',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.primary,
-                fontWeight: FontWeight.bold,
-              ),
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
             ),
             const SizedBox(height: 8),
             for (var step in meal.steps)
               Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Text(
                   step,
                   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
                   textAlign: TextAlign.center,
                 ),
               ),
